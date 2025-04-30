5e0d6d49f29b2215f33fa22bed46b654022b0d5b
Yash Vyas <yjvyas@gmail.com>
2025-04-30 11:17:27 +0200

    Changed gravity to lunar conditions

--- towr/src/dynamic_model.cc
+++ towr/src/dynamic_model.cc
@@ -34,7 +34,8 @@ namespace towr {
 DynamicModel::DynamicModel(double mass, int ee_count)
 {
   m_ = mass;
-  g_ = 9.80665;
+  // g_ = 9.80665;
+  g_ = 1.62;
 
   com_pos_.setZero();
   com_acc_.setZero();
